e7e8e6f5c488900cb24984816d88474adedb15c3
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-03 12:13:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+add = num1 +num2
+return add
 end
 
 def subtraction(num1, num2)
-
+subb = num1-num2
+return subb
 end
 
 def division(num1, num2)
 
+div = num1/num2
+return div
 end
 
 def multiplication(num1, num2)
-
+multi = num1*num2
+return multi
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
 end
 
 def square_root(num)
-
+squ = Math.sqrt(num)
 end
